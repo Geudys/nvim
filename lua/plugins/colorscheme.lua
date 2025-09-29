fb10f24bf0ab8519f4f4798df94bf427c75d54a5
Geudys Pérez Santiago <geudysperezstgo@gmail.com>
2025-09-28 21:12:27 -0400

    Incorparation nord theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
-	"oxfist/night-owl.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		require("night-owl").setup()
-		vim.cmd.colorscheme("night-owl")
-	end,
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("night-owl").setup()
+		end,
+	},
+	{
+		"shaunsingh/nord.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			vim.g.nord_contrast = true
+			vim.g.nord_borders = true
+			vim.g.nord_disable_background = true
+			vim.g.nord_italic = true
+			vim.g.nord_uniform_diff_background = true
+			vim.g.nord_bold = true
+		end,
+	},
 }
