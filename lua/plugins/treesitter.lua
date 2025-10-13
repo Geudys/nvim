b77b42026a0734fe806744ecf134b5093b27f7fb
Geudys Pérez Santiago <geudysperezstgo@gmail.com>
2025-10-12 19:37:39 -1200

    Changes treesiter
    Add new option in the treesiter
    Chages option en treesiter contex

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,88 +1,84 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate", -- para asegurar instalación
-		lazy = false,
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				highlight = { enable = true },
-				indent = { enable = true },
 				ensure_installed = {
 					"json",
 					"javascript",
 					"typescript",
 					"typst",
-					"tsx",
 					"yaml",
 					"html",
 					"css",
-					"prisma",
 					"markdown",
 					"markdown_inline",
-					"svelte",
 					"graphql",
 					"bash",
 					"lua",
 					"vim",
-					"dockerfile",
 					"gitignore",
 					"query",
 					"vimdoc",
-					"c",
 				},
-				incremental_selection = {
+				sync_install = false,
+				auto_install = true,
+				indent = {
 					enable = true,
-					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = "<C-s>",
-						node_decremental = "<bs>",
-					},
 				},
-				autotag = { enable = true },
-				playground = { enable = true },
-				rainbow = { enable = true, extended_mode = true },
-				context_commentstring = { enable = true, enable_autocmd = false },
+
+				highlight = {
+					enable = true,
+					disable = function(lang, buf)
+						if lang == "html" then
+							print("disabled")
+							return true
+						end
+
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							vim.notify(
+								"File larger than 100KB treesitter disabled for performance",
+								vim.log.levels.WARN,
+								{ title = "Treesitter" }
+							)
+							return true
+						end
+					end,
+					additional_vim_regex_highlighting = { "markdown" },
+				},
 			})
 
-			-- Registrar zsh con el parser de bash
-			vim.treesitter.language.register("bash", "zsh")
+			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			treesitter_parser_config.templ = {
+				install_info = {
+					url = "https://github.com/vrischmann/tree-sitter-templ.git",
+					files = { "src/parser.c", "src/scanner.c" },
+					branch = "master",
+				},
+			}
 
-			-- Playground toggle
-			vim.keymap.set("n", "<leader>tp", ":TSPlaygroundToggle<CR>", { desc = "Toggle TS Playground" })
+			vim.treesitter.language.register("templ", "templ")
 		end,
 	},
 
 	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		"nvim-treesitter/nvim-treesitter-context",
+		after = "nvim-treesitter",
 		config = function()
-			-- Text objects config
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-							["al"] = "@loop.outer",
-							["il"] = "@loop.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]f"] = "@function.outer",
-						},
-						goto_previous_start = {
-							["[f"] = "@function.outer",
-						},
-					},
-				},
+			require("treesitter-context").setup({
+				enable = true,
+				multiwindow = false,
+				max_lines = 0,
+				min_window_height = 0,
+				line_numbers = true,
+				multiline_threshold = 20,
+				trim_scope = "outer",
+				mode = "cursor",
+				separator = nil,
+				zindex = 20,
+				on_attach = nil,
 			})
 		end,
 	},
