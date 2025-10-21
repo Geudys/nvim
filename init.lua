88938c8600ec7e27fd1fe3db7d1d8424d2510e62
Geudys Pérez Santiago <geudysperezstgo@gmail.com>
2025-10-20 14:04:07 -1200

    remove-commets-init.lua
    Revome all commets in init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,7 @@
--- ========================================
--- Configuración Global
--- ========================================
-
--- Tecla líder
 vim.g.mapleader = " "
 
--- Soporte para colores en terminal
 vim.opt.termguicolors = true
 
--- ========================================
--- Instalar Lazy automáticamente si no está
--- ========================================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -18,19 +9,13 @@ if not vim.loop.fs_stat(lazypath) then
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- última versión estable
+		"--branch=stable",
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- ========================================
--- Cargar Plugins con Lazy
--- ========================================
 require("lazy").setup("plugins")
 
--- ========================================
--- Opciones y Atajos
--- ========================================
-require("core.options") -- Opciones generales de Neovim
-require("core.keymaps") -- Atajos personalizados
+require("core.options")
+require("core.keymaps")
