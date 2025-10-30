0102d7ac1900072ac758cfce88e139473e23b9ce
Geudys <geudysperezstgo@gmail.com>
2025-10-30 19:58:00 -0400

    change-repositori-obsidian-plugins

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,11 +1,12 @@
 return {
     {
-        "obsidian-nvim/obsidian.nvim",
+        "epwalsh/obsidian.nvim",
+        version = "*", -- usa la última versión estable
         lazy = true,
         event = "VeryLazy",
-        version = "*",
         ft = "markdown",
         dependencies = { "nvim-lua/plenary.nvim" },
+
         opts = {
             workspaces = {
                 {
@@ -14,18 +15,36 @@ return {
                 },
             },
 
-            completion = { nvim_cmp = true, min_chars = 3 },
-            finder = { max_results = 50 },
-            picker = { name = "telescope.nvim" },
-            ui = { enable = true },
-            frontmatter = { enable = true },
+            completion = {
+                nvim_cmp = true,
+                min_chars = 3,
+            },
+
+            finder = {
+                max_results = 50,
+            },
+
+            picker = {
+                name = "telescope.nvim",
+            },
+
+            ui = {
+                enable = true,
+            },
+
+            disable_frontmatter = true, -- ✅ no agrega id, aliases ni tags automáticos
+
+            note_id_func = function(title)
+                return title
+            end,
+
             footer = {
                 enabled = false,
                 separator = false,
-                format = "{{backlinks}} backlinks  {{properties}} properties  {{words}} words  {{chars}} chars",
-                hl_group = "@property",
             },
+
             legacy_commands = false,
+
             templates = {
                 subdir = "99 - Templates",
                 date_format = "%Y-%m-%d",
@@ -48,6 +67,7 @@ return {
             local obsidian = require("obsidian")
             obsidian.setup(opts)
 
+            -- Ajuste visual: oculta caracteres markdown
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = "markdown",
                 callback = function()
@@ -55,39 +75,38 @@ return {
                 end,
             })
 
+            -- Mapeos personalizados
             local map = vim.keymap.set
             local key_opts = { noremap = true, silent = true }
 
-            map("n", "<leader>on", ":Obsidian new<CR>", key_opts)
-            map("n", "<leader>oo", ":Obsidian open<CR>", key_opts)
-            map("n", "<leader>os", ":Obsidian search<CR>", key_opts)
-            map("n", "<leader>ol", ":Obsidian link<CR>", key_opts)
-            map("n", "<leader>ob", ":Obsidian backlinks<CR>", key_opts)
-            map("n", "<leader>ot", ":Obsidian template<CR>", key_opts)
+            map("n", "<leader>on", ":ObsidianNew<CR>", key_opts)
+            map("n", "<leader>oo", ":ObsidianQuickSwitch<CR>", key_opts)
+            map("n", "<leader>os", ":ObsidianSearch<CR>", key_opts)
+            map("n", "<leader>ol", ":ObsidianLink<CR>", key_opts)
+            map("n", "<leader>ob", ":ObsidianBacklinks<CR>", key_opts)
+            map("n", "<leader>ot", ":ObsidianTemplate<CR>", key_opts)
 
+            -- Daily note personalizada
             map("n", "<leader>od", function()
-                local daily_dir = "/mnt/c/My Desktop/GPS/Obsidian/06 - Daily/Daily 2025/Daily 2025-10/"
+                local daily_dir = "/home/geudys/Escritorio/Obsidian/06 - Daily/Daily 2025/Daily 2025-10/"
                 local filename = "Daily " .. os.date("%Y-%m-%d") .. ".md"
                 local filepath = daily_dir .. filename
-
                 if vim.fn.filereadable(filepath) == 0 then
                     vim.fn.writefile({}, filepath)
                     vim.notify("🗓️ Nueva Daily Note creada: " .. filename, vim.log.levels.INFO)
                 else
                     vim.notify("✅ Ya existe la Daily Note de hoy.", vim.log.levels.INFO)
                 end
-
                 vim.cmd("edit " .. filepath)
             end, { desc = "ObsidianDaily" }, key_opts)
+
+            -- Nota rápida única
             map("n", "<leader>ou", function()
                 local Path = require("plenary.path")
-
-                local target_dir = "/mnt/c/My Desktop/GPS/Obsidian/"
+                local target_dir = "/home/geudys/Escritorio/Obsidian/"
                 local filename = "- " .. os.date("%Y%m%d%H%M") .. ".md"
                 local filepath = Path:new(target_dir .. filename)
-
                 vim.cmd("edit " .. filepath.filename)
-
                 vim.notify("📝 Nueva nota creada: " .. filename, vim.log.levels.INFO)
             end, { desc = "ObsidianNote" }, key_opts)
         end,
