442bf0b00484b7869f5f73e1edcebfaed72dca41
Geudys <geudysperezstgo@gmail.com>
2025-11-01 09:43:10 -0400

    change-ident-neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,27 +1,27 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		lazy = false,
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("neo-tree").setup({
-				filesystem = {
-					filtered_items = {
-						visible = true,
-						hide_dotfiles = false,
-						hide_gitignored = false,
-					},
-				},
-			})
-			vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
-			vim.keymap.set("n", "<leader>E", ":Neotree focus<CR>", { desc = "Focus Neo-tree" })
-			vim.keymap.set("n", "<leader>ge", ":Neotree git_status<CR>", { desc = "Git status en Neo-tree" })
-			vim.keymap.set("n", "<leader>be", ":Neotree buffers<CR>", { desc = "Buffers en Neo-tree" })
-		end,
-	},
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        lazy = false,
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup({
+                filesystem = {
+                    filtered_items = {
+                        visible = true,
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                    },
+                },
+            })
+            vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+            vim.keymap.set("n", "<leader>E", ":Neotree focus<CR>", { desc = "Focus Neo-tree" })
+            vim.keymap.set("n", "<leader>ge", ":Neotree git_status<CR>", { desc = "Git status en Neo-tree" })
+            vim.keymap.set("n", "<leader>be", ":Neotree buffers<CR>", { desc = "Buffers en Neo-tree" })
+        end,
+    },
 }
