379109fc86f7c99407dda3817a98216ecd6e5708
Geudys <geudysperezstgo@gmail.com>
2025-11-04 22:23:19 -0400

    change-keymap-"e"-files

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,34 +1,37 @@
 return {
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.dashboard")
+    {
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
 
-			dashboard.section.header.val = {
-				[[                                                                      ]],
-				[[       ████ ██████           █████      ██                     ]],
-				[[      ███████████             █████                             ]],
-				[[      █████████ ███████████████████ ███   ███████████   ]],
-				[[     █████████  ███    █████████████ █████ ██████████████   ]],
-				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			}
+            dashboard.section.header.val = {
+                [[                                                                      ]],
+                [[       ████ ██████           █████      ██                     ]],
+                [[      ███████████             █████                             ]],
+                [[      █████████ ███████████████████ ███   ███████████   ]],
+                [[     █████████  ███    █████████████ █████ ██████████████   ]],
+                [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+                [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+                [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            }
 
-			dashboard.section.buttons.val = {
-				dashboard.button("e", "  Neotre", ":Neotree<CR>"),
-				dashboard.button("f", "  Search files", ":Telescope find_files<CR>"),
-				dashboard.button("r", "  Recents", ":Telescope oldfiles<CR>"),
-				dashboard.button("h", "  Check health", ":checkhealth<CR>"),
-				dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
-				dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
-				dashboard.button("q", "  Exit", ":qa<CR>"),
-			}
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  Mini files",
+                    function()
+                        require("mini.files").open(vim.api.nvim_buf_get_name(0))
+                    end),
+                dashboard.button("f", "  Search files", ":Telescope find_files<CR>"),
+                dashboard.button("r", "  Recents", ":Telescope oldfiles<CR>"),
+                dashboard.button("h", "  Check health", ":checkhealth<CR>"),
+                dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+                dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
+                dashboard.button("q", "  Exit", ":qa<CR>"),
+            }
 
-			dashboard.opts.opts.noautocmd = true
-			alpha.setup(dashboard.config)
-		end,
-	},
+            dashboard.opts.opts.noautocmd = true
+            alpha.setup(dashboard.config)
+        end,
+    },
 }
