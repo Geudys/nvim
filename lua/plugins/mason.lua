1a74fbf7467a7da160486439733f1522b6835857
Geudys <geudysperezstgo@gmail.com>
2025-11-04 21:25:16 -0400

    off-marksman-lsp-server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,60 +1,60 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"emmet_ls",
-					"pyright",
-					"eslint",
-					"lua_ls",
-					"rust_analyzer",
-					"ts_ls",
-					"html",
-					"cssls",
-					"jsonls",
-					"vimls",
-					"marksman",
-				},
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"prettier",
-					"prettierd",
-					"stylua",
-					"isort",
-					"black",
-					"pylint",
-					"eslint_d",
-				},
-			})
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "emmet_ls",
+                    "pyright",
+                    "eslint",
+                    "lua_ls",
+                    "rust_analyzer",
+                    "ts_ls",
+                    "html",
+                    "cssls",
+                    "jsonls",
+                    "vimls",
+                    -- "marksman",
+                },
+            })
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "prettier",
+                    "prettierd",
+                    "stylua",
+                    "isort",
+                    "black",
+                    "pylint",
+                    "eslint_d",
+                },
+            })
+        end,
+    },
 }
