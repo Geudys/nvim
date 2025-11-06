83bb15a46ff742115e787ddb2ade46a4fae1c572
Geudys <geudysperezstgo@gmail.com>
2025-11-05 20:40:32 -0400

    delete-theme-nord

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -24,7 +24,7 @@ vim.opt.backup = false
 
 local undodir = vim.fn.expand("~/.vim/undodir")
 if vim.fn.isdirectory(undodir) == 0 then
-	vim.fn.mkdir(undodir, "p")
+    vim.fn.mkdir(undodir, "p")
 end
 vim.opt.undodir = undodir
 vim.opt.undofile = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,17 @@
 return {
-	{
-		"oxfist/night-owl.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local night_owl = require("night-owl")
-			night_owl.setup({
-				bold = true,
-				italics = true,
-				underline = true,
-				undercurl = true,
-				transparent_background = true,
-			})
-		end,
-	},
-	{
-		"shaunsingh/nord.nvim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			vim.g.nord_contrast = true
-			vim.g.nord_borders = true
-			vim.g.nord_disable_background = true
-			vim.g.nord_italic = true
-			vim.g.nord_uniform_diff_background = true
-			vim.g.nord_bold = true
-		end,
-	},
+    {
+        "oxfist/night-owl.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local night_owl = require("night-owl")
+            night_owl.setup({
+                bold = true,
+                italics = true,
+                underline = true,
+                undercurl = true,
+                transparent_background = true,
+            })
+        end,
+    },
 }
