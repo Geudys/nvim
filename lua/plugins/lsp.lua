6f766af3c67c6a1398cfd1d8fb9bc0263ba25e2f
Geudys <geudysperezstgo@gmail.com>
2025-11-11 22:06:16 -0400

    remove-marksman-config
    revome: marksman config lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,116 +1,102 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
-	},
-	config = function()
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local keymap = vim.keymap
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+        { "folke/neodev.nvim",                   opts = {} },
+    },
+    config = function()
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local keymap = vim.keymap
+        local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = { buffer = ev.buf, silent = true }
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                local opts = { buffer = ev.buf, silent = true }
 
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                opts.desc = "Show LSP references"
+                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                opts.desc = "Go to declaration"
+                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+                opts.desc = "Show LSP definitions"
+                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
-				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+                opts.desc = "Show LSP implementations"
+                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+                opts.desc = "Show LSP type definitions"
+                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-				opts.desc = "Code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                opts.desc = "Code actions"
+                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-				opts.desc = "Rename symbol"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                opts.desc = "Rename symbol"
+                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-				opts.desc = "Buffer diagnostics"
-				keymap.set("n", "<leader>rD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+                opts.desc = "Buffer diagnostics"
+                keymap.set("n", "<leader>rD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-				opts.desc = "Line diagnostics"
-				keymap.set("n", "<leader>rd", vim.diagnostic.open_float, opts)
+                opts.desc = "Line diagnostics"
+                keymap.set("n", "<leader>rd", vim.diagnostic.open_float, opts)
 
-				opts.desc = "Prev diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                opts.desc = "Prev diagnostic"
+                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
-				opts.desc = "Next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                opts.desc = "Next diagnostic"
+                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-				opts.desc = "Hover docs"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                opts.desc = "Hover docs"
+                keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-			end,
-		})
+                opts.desc = "Restart LSP"
+                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+            end,
+        })
 
-		vim.diagnostic.config({
-			virtual_text = {
-				enabled = true,
-				source = "if_many",
-				spacing = 4,
-				prefix = "●",
-				format = function(diagnostic)
-					return diagnostic.message
-				end,
-			},
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = " ",
-					[vim.diagnostic.severity.WARN] = " ",
-					[vim.diagnostic.severity.HINT] = "󰠠 ",
-					[vim.diagnostic.severity.INFO] = " ",
-				},
-			},
-			float = {
-				enabled = true,
-				source = "always",
-				border = "rounded",
-			},
-			update_in_insert = false,
-			severity_sort = true,
-		})
+        vim.diagnostic.config({
+            virtual_text = {
+                enabled = true,
+                source = "if_many",
+                spacing = 4,
+                prefix = "●",
+                format = function(diagnostic)
+                    return diagnostic.message
+                end,
+            },
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = "󰠠 ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+            },
+            float = {
+                enabled = true,
+                source = "always",
+                border = "rounded",
+            },
+            update_in_insert = false,
+            severity_sort = true,
+        })
 
-		vim.lsp.config("emmet_ls", {
-			capabilities = capabilities,
-			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
-		})
+        vim.lsp.config("emmet_ls", {
+            capabilities = capabilities,
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+        })
 
-		vim.lsp.config("lua_ls", {
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = { globals = { "vim" } },
-					completion = { callSnippet = "Replace" },
-				},
-			},
-		})
-
-		vim.lsp.config("marksman", {
-			capabilities = capabilities,
-			filetypes = { "markdown", "markdown.mdx" },
-			settings = {
-				markdown = {
-					completion = {
-						enable = true,
-						show_links = true,
-						show_headers = true,
-					},
-				},
-			},
-		})
-	end,
+        vim.lsp.config("lua_ls", {
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = { globals = { "vim" } },
+                    completion = { callSnippet = "Replace" },
+                },
+            },
+        })
+    end,
 }
