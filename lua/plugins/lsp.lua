599489ad5820251f27f4ce46142e99c9ad94a506
Geudys <geudysperezstgo@gmail.com>
2025-11-11 22:39:30 -0400

    add-new-features-lsp-keymaps
    add: new keymaps
    add: new config lsp server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,95 +8,212 @@ return {
     },
     config = function()
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local keymap = vim.keymap
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
         vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
             callback = function(ev)
                 local opts = { buffer = ev.buf, silent = true }
+                local keymap = vim.keymap.set
 
-                opts.desc = "Show LSP references"
-                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-
-                opts.desc = "Go to declaration"
-                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-                opts.desc = "Show LSP definitions"
-                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-
-                opts.desc = "Show LSP implementations"
-                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-
-                opts.desc = "Show LSP type definitions"
-                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-
-                opts.desc = "Code actions"
-                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-                opts.desc = "Rename symbol"
-                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
-                opts.desc = "Buffer diagnostics"
-                keymap.set("n", "<leader>rD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-
-                opts.desc = "Line diagnostics"
-                keymap.set("n", "<leader>rd", vim.diagnostic.open_float, opts)
-
-                opts.desc = "Prev diagnostic"
-                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-                opts.desc = "Next diagnostic"
-                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-
-                opts.desc = "Hover docs"
-                keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-                opts.desc = "Restart LSP"
-                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+                keymap("n", "gR", "<cmd>Telescope lsp_references<CR>",
+                    vim.tbl_extend("force", opts, { desc = "Show LSP references" }))
+                keymap("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to declaration" }))
+                keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>",
+                    vim.tbl_extend("force", opts, { desc = "Show LSP definitions" }))
+                keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>",
+                    vim.tbl_extend("force", opts, { desc = "Show LSP implementations" }))
+                keymap("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>",
+                    vim.tbl_extend("force", opts, { desc = "Show LSP type definitions" }))
+                keymap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action,
+                    vim.tbl_extend("force", opts, { desc = "Code actions" }))
+                keymap("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+                keymap("n", "<leader>rD", "<cmd>Telescope diagnostics bufnr=0<CR>",
+                    vim.tbl_extend("force", opts, { desc = "Buffer diagnostics" }))
+                keymap("n", "<leader>rd", vim.diagnostic.open_float,
+                    vim.tbl_extend("force", opts, { desc = "Line diagnostics" }))
+                keymap("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Prev diagnostic" }))
+                keymap("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
+                keymap("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover docs" }))
+                keymap("n", "<leader>rs", "<cmd>LspRestart<CR>", vim.tbl_extend("force", opts, { desc = "Restart LSP" }))
             end,
         })
 
         vim.diagnostic.config({
             virtual_text = {
-                enabled = true,
-                source = "if_many",
                 spacing = 4,
                 prefix = "●",
-                format = function(diagnostic)
-                    return diagnostic.message
-                end,
+                severity = { min = vim.diagnostic.severity.HINT },
             },
             signs = {
                 text = {
-                    [vim.diagnostic.severity.ERROR] = " ",
-                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
                     [vim.diagnostic.severity.HINT] = "󰠠 ",
-                    [vim.diagnostic.severity.INFO] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
                 },
             },
             float = {
-                enabled = true,
                 source = "always",
                 border = "rounded",
+                header = "",
+                prefix = "",
             },
             update_in_insert = false,
             severity_sort = true,
+            underline = true,
         })
 
         vim.lsp.config("emmet_ls", {
             capabilities = capabilities,
-            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte", "vue" },
         })
 
         vim.lsp.config("lua_ls", {
             capabilities = capabilities,
             settings = {
                 Lua = {
+                    runtime = { version = "LuaJIT" },
                     diagnostics = { globals = { "vim" } },
                     completion = { callSnippet = "Replace" },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = { vim.env.VIMRUNTIME },
+                    },
+                    telemetry = { enable = false },
                 },
             },
+        })
+
+        vim.lsp.config("pyright", {
+            capabilities = capabilities,
+            settings = {
+                python = {
+                    analysis = {
+                        typeCheckingMode = "basic",
+                        autoSearchPaths = true,
+                        useLibraryCodeForTypes = true,
+                        diagnosticMode = "workspace",
+                    },
+                },
+            },
+        })
+
+        vim.lsp.config("eslint", {
+            capabilities = capabilities,
+            on_attach = function(_, bufnr)
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    buffer = bufnr,
+                    command = "EslintFixAll",
+                })
+            end,
+        })
+
+        vim.lsp.config("rust_analyzer", {
+            capabilities = capabilities,
+            settings = {
+                ["rust-analyzer"] = {
+                    checkOnSave = {
+                        command = "clippy",
+                    },
+                    cargo = {
+                        allFeatures = true,
+                    },
+                    procMacro = {
+                        enable = true,
+                    },
+                },
+            },
+        })
+
+        vim.lsp.config("ts_ls", {
+            capabilities = capabilities,
+            settings = {
+                typescript = {
+                    inlayHints = {
+                        includeInlayParameterNameHints = "all",
+                        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                        includeInlayFunctionParameterTypeHints = true,
+                        includeInlayVariableTypeHints = true,
+                        includeInlayPropertyDeclarationTypeHints = true,
+                        includeInlayFunctionLikeReturnTypeHints = true,
+                        includeInlayEnumMemberValueHints = true,
+                    },
+                },
+                javascript = {
+                    inlayHints = {
+                        includeInlayParameterNameHints = "all",
+                        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                        includeInlayFunctionParameterTypeHints = true,
+                        includeInlayVariableTypeHints = true,
+                        includeInlayPropertyDeclarationTypeHints = true,
+                        includeInlayFunctionLikeReturnTypeHints = true,
+                        includeInlayEnumMemberValueHints = true,
+                    },
+                },
+            },
+        })
+
+        vim.lsp.config("html", {
+            capabilities = capabilities,
+            filetypes = { "html", "templ" },
+        })
+
+        vim.lsp.config("cssls", {
+            capabilities = capabilities,
+            settings = {
+                css = {
+                    validate = true,
+                    lint = {
+                        unknownAtRules = "ignore",
+                    },
+                },
+                scss = {
+                    validate = true,
+                    lint = {
+                        unknownAtRules = "ignore",
+                    },
+                },
+                less = {
+                    validate = true,
+                    lint = {
+                        unknownAtRules = "ignore",
+                    },
+                },
+            },
+        })
+
+        vim.lsp.config("jsonls", {
+            capabilities = capabilities,
+            settings = {
+                json = {
+                    validate = { enable = true },
+                },
+            },
+        })
+
+        vim.lsp.config("bashls", {
+            capabilities = capabilities,
+            filetypes = { "sh", "bash" },
+        })
+
+        vim.lsp.config("fish_lsp", {
+            capabilities = capabilities,
+            filetypes = { "fish" },
+        })
+
+        vim.lsp.enable({
+            "emmet_ls",
+            "pyright",
+            "eslint",
+            "lua_ls",
+            "rust_analyzer",
+            "ts_ls",
+            "html",
+            "cssls",
+            "jsonls",
+            "bashls",
+            "fish_lsp",
         })
     end,
 }
