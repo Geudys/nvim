46d4d1a8d4a76541df0bc8a84ed79733e546cf62
Geudys <geudysperezstgo@gmail.com>
2025-11-11 22:15:18 -0400

    refactory-load-lazy
    move: load lazy to lua/core/lazy.lua
    remove: load lazy in the init.lua

--- init.lua
+++ init.lua
@@ -2,20 +2,7 @@ vim.g.mapleader = " "
 
 vim.opt.termguicolors = true
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
 
 require("core.options")
+require("core.lazy")
 require("core.keymaps")
