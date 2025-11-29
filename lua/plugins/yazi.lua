6d3b2fc9ab03f61e957bc5a7aa2fe11240c13c8f
Geudys <geudysperezstgo@gmail.com>
2025-11-28 23:09:42 -0400

    add-yazi-file-manager

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,11 @@
+return {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+        {
+            "<leader>e",
+            "<cmd>Yazi<cr>",
+            desc = "Open yazi at the current file",
+        },
+    },
+}
