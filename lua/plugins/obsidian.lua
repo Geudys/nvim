467cf7578390e29280006f27b8c8b03f38a9e70c
Geudys <geudysperezstgo@gmail.com>
2025-12-07 22:19:09 -0400

    add-render-markdown
    add: render markdown plugins
    add: config render especial checkbox obsidian plugins

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,106 +1,121 @@
 return {
-    {
-        "obsidian-nvim/obsidian.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        ft = "markdown",
-        dependencies = { "nvim-lua/plenary.nvim" },
+	{
+		"obsidian-nvim/obsidian.nvim",
+		lazy = true,
+		event = "VeryLazy",
+		ft = "markdown",
+		dependencies = { "nvim-lua/plenary.nvim" },
 
-        opts = {
-            workspaces = {
-                {
-                    name = "Obsidian",
-                    path = "/home/geudys/Desktop/Obsidian/",
-                },
-            },
+		opts = {
+			workspaces = {
+				{
+					name = "Obsidian",
+					path = "/home/geudys/Desktop/Obsidian/",
+				},
+			},
 
-            completion = {
-                nvim_cmp = true,
-                min_chars = 3,
-            },
+			completion = {
+				nvim_cmp = true,
+				min_chars = 3,
+			},
 
-            finder = {
-                max_results = 50,
-            },
+			finder = {
+				max_results = 50,
+			},
 
-            picker = {
-                name = "telescope.nvim",
-            },
+			picker = {
+				name = "telescope.nvim",
+			},
 
-            ui = {
-                enable = true,
-            },
+			ui = {
+				enable = true,
+			},
 
-            frontmatter = {
-                enabled = false
-            },
+			frontmatter = {
+				enabled = false,
+			},
 
-            footer = {
-                enabled = false,
-                separator = false,
-            },
+			footer = {
+				enabled = false,
+				separator = false,
+			},
 
-            legacy_commands = false,
+			legacy_commands = false,
 
-            templates = {
-                subdir = "99 - Templates",
-                date_format = "%Y-%m-%d",
-                time_format = "%H:%M",
-                substitutions = {
-                    Date = function()
-                        return os.date("%Y-%m-%d")
-                    end,
-                    Time = function()
-                        return os.date("%H:%M")
-                    end,
-                    ["Date:YYYYMMDDHHmm"] = function()
-                        return os.date("%Y%m%d%H%M")
-                    end,
-                },
-            },
-        },
+			templates = {
+				subdir = "99 - Templates",
+				date_format = "%Y-%m-%d",
+				time_format = "%H:%M",
+				substitutions = {
+					Date = function()
+						return os.date("%Y-%m-%d")
+					end,
+					Time = function()
+						return os.date("%H:%M")
+					end,
+					["Date:YYYYMMDDHHmm"] = function()
+						return os.date("%Y%m%d%H%M")
+					end,
+				},
+			},
+		},
 
-        config = function(_, opts)
-            local obsidian = require("obsidian")
-            obsidian.setup(opts)
+		config = function(_, opts)
+			local obsidian = require("obsidian")
+			obsidian.setup(opts)
 
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = "markdown",
-                callback = function()
-                    vim.opt_local.conceallevel = 2
-                end,
-            })
-            local map = vim.keymap.set
-            local key_opts = { noremap = true, silent = true }
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown",
+				callback = function()
+					vim.opt_local.conceallevel = 2
+				end,
+			})
+			local map = vim.keymap.set
+			local key_opts = { noremap = true, silent = true }
 
-            map("n", "<leader>on", ":Obsidian new<CR>", key_opts)
-            map("n", "<leader>oo", ":Obsidian quickSwitch<CR>", key_opts)
-            map("n", "<leader>os", ":Obsidian search<CR>", key_opts)
-            map("n", "<leader>ol", ":Obsidian link<CR>", key_opts)
-            map("n", "<leader>ob", ":Obsidian backlinks<CR>", key_opts)
-            map("n", "<leader>ot", ":Obsidian template<CR>", key_opts)
+			map("n", "<leader>on", ":Obsidian new<CR>", key_opts)
+			map("n", "<leader>oo", ":Obsidian quickSwitch<CR>", key_opts)
+			map("n", "<leader>os", ":Obsidian search<CR>", key_opts)
+			map("n", "<leader>ol", ":Obsidian link<CR>", key_opts)
+			map("n", "<leader>ob", ":Obsidian backlinks<CR>", key_opts)
+			map("n", "<leader>ot", ":Obsidian template<CR>", key_opts)
 
-            map("n", "<leader>od", function()
-                local daily_dir = "/home/geudys/Desktop/Obsidian/06 - Greed/Daily 2025/Daily 2025-12/"
-                local filename = "Daily " .. os.date("%Y-%m-%d") .. ".md"
-                local filepath = daily_dir .. filename
-                if vim.fn.filereadable(filepath) == 0 then
-                    vim.fn.writefile({}, filepath)
-                    vim.notify("🗓️ Nueva Daily Note creada: " .. filename, vim.log.levels.INFO)
-                else
-                    vim.notify("✅ Ya existe la Daily Note de hoy.", vim.log.levels.INFO)
-                end
-                vim.cmd("edit " .. filepath)
-            end, { desc = "Obsidian daily" })
+			map("n", "<leader>od", function()
+				local daily_dir = "/home/geudys/Desktop/Obsidian/06 - Greed/Daily 2025/Daily 2025-12/"
+				local filename = "Daily " .. os.date("%Y-%m-%d") .. ".md"
+				local filepath = daily_dir .. filename
+				if vim.fn.filereadable(filepath) == 0 then
+					vim.fn.writefile({}, filepath)
+					vim.notify("🗓️ Nueva Daily Note creada: " .. filename, vim.log.levels.INFO)
+				else
+					vim.notify("✅ Ya existe la Daily Note de hoy.", vim.log.levels.INFO)
+				end
+				vim.cmd("edit " .. filepath)
+			end, { desc = "Obsidian daily" })
 
-            map("n", "<leader>ou", function()
-                local Path = require("plenary.path")
-                local target_dir = "/home/geudys/Desktop/Obsidian/"
-                local filename = "- " .. os.date("%Y%m%d%H%M") .. ".md"
-                local filepath = Path:new(target_dir .. filename)
-                vim.cmd("edit " .. filepath.filename)
-                vim.notify("📝 Nueva nota creada: " .. filename, vim.log.levels.INFO)
-            end, { desc = "Obsidian note" })
-        end,
-    },
+			map("n", "<leader>ou", function()
+				local Path = require("plenary.path")
+				local target_dir = "/home/geudys/Desktop/Obsidian/"
+				local filename = "- " .. os.date("%Y%m%d%H%M") .. ".md"
+				local filepath = Path:new(target_dir .. filename)
+				vim.cmd("edit " .. filepath.filename)
+				vim.notify("📝 Nueva nota creada: " .. filename, vim.log.levels.INFO)
+			end, { desc = "Obsidian note" })
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			render_modes = { "n", "v", "i", "c" },
+			checkbox = {
+				unchecked = { icon = " " },
+				checked = { icon = " " },
+				custom = {
+					{ raw = "[>]", rendered = " " },
+					{ raw = "[!]", rendered = " " },
+					{ raw = "[~]", rendered = " " },
+				},
+			},
+		},
+	},
 }
