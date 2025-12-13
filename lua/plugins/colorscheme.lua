5ac16e0047fa410155350e3ea06c664d95829c28
Geudys <geudysperezstgo@gmail.com>
2025-12-13 15:32:52 -0400

    update-theme-config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,18 @@
 return {
-    {
-        "oxfist/night-owl.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            local night_owl = require("night-owl")
-            night_owl.setup({
-                bold = true,
-                italics = true,
-                underline = true,
-                undercurl = true,
-                transparent_background = true,
-            })
-        end,
-    },
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local night_owl = require("night-owl")
+			night_owl.setup({
+				bold = true,
+				italics = true,
+				underline = true,
+				undercurl = true,
+				transparent_background = true,
+			})
+			vim.cmd.colorscheme("night-owl")
+		end,
+	},
 }
