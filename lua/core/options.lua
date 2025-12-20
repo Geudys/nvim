d33a6880fef5bb88ed8f2cf85e56538d880c6abc
Geudys <geudysperezstgo@gmail.com>
2025-12-19 20:20:19 -0400

    update-options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,17 +1,31 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+
 vim.opt.fillchars:append({ eob = " " })
+
 vim.opt.wrap = true
 vim.opt.linebreak = true
-vim.opt.spell = true
-vim.opt.spelllang = "es,en"
 vim.opt.breakindent = true
-vim.opt.scrolloff = 20
-vim.opt.sidescrolloff = 20
-vim.opt.cursorline = true
+vim.opt.showbreak = "↳ "
+
+vim.opt.scrolloff = 30
+vim.opt.sidescrolloff = 5
+
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+vim.opt.spell = true
+vim.opt.spelllang = { "es", "en" }
+
+vim.opt.formatoptions:append("t")
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -24,18 +38,19 @@ vim.opt.backup = false
 
 local undodir = vim.fn.expand("~/.vim/undodir")
 if vim.fn.isdirectory(undodir) == 0 then
-    vim.fn.mkdir(undodir, "p")
+	vim.fn.mkdir(undodir, "p")
 end
+
 vim.opt.undodir = undodir
 vim.opt.undofile = true
 
-vim.opt.isfname:append("@-@")
 vim.opt.encoding = "utf-8"
 vim.opt.mouse = "a"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.hidden = true
+vim.opt.confirm = true
 
 pcall(vim.cmd.colorscheme, "night-owl")
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
