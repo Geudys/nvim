b279280bfade3f55de207cb802f970d1fdd2960b
Geudys <geudysperezstgo@gmail.com>
2026-01-04 23:32:40 -0400

    update-cmp-config
    add: icons for sugenstion list

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -26,6 +26,34 @@ return {
 					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
+			local kind_icons = {
+				Text = "",
+				Method = "󰆧",
+				Function = "󰊕",
+				Constructor = "",
+				Field = "󰇽",
+				Variable = "󰂡",
+				Class = "󰠱",
+				Interface = "",
+				Module = "",
+				Property = "󰜢",
+				Unit = "",
+				Value = "󰎠",
+				Enum = "",
+				Keyword = "󰌋",
+				Snippet = "",
+				Color = "󰏘",
+				File = "󰈙",
+				Reference = "󰈇",
+				Folder = "󰉋",
+				EnumMember = "",
+				Constant = "󰏿",
+				Struct = "󰙅",
+				Event = "",
+				Operator = "󰆕",
+				TypeParameter = "",
+			}
+
 			cmp.setup({
 				window = {
 					completion = cmp.config.window.bordered(),
@@ -70,11 +98,19 @@ return {
 					end, { "i", "s" }),
 				}),
 
+				formatting = {
+					fields = { "abbr", "kind" },
+					format = function(_, vim_item)
+						vim_item.kind = kind_icons[vim_item.kind] or ""
+						return vim_item
+					end,
+				},
+
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "path" },
-					{ name = "buffer", keyword_length = 3 },
+					{ name = "buffer" },
 				}),
 
 				experimental = {
