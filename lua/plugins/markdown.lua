169273b3adf58fde346a6fd4a389ab527e3df4e5
Geudys <geudysperezstgo@gmail.com>
2026-01-10 22:27:23 -0400

    change-name-obsidian
    change: obsidian -> markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,51 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			render_modes = { "n", "v", "i", "c" },
+
+			checkbox = {
+				unchecked = { icon = " " },
+				checked = { icon = " " },
+
+				custom = {
+					info = {
+						raw = "[>]",
+						rendered = " ",
+						highlight = "RenderMarkdownInfo",
+					},
+					warn = {
+						raw = "[!]",
+						rendered = " ",
+						highlight = "RenderMarkdownWarn",
+					},
+					error = {
+						raw = "[~]",
+						rendered = " ",
+						highlight = "RenderMarkdownError",
+					},
+				},
+			},
+
+			html = { enabled = false },
+			latex = { enabled = false },
+			yaml = { enabled = false },
+		},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		event = "VeryLazy",
+		ft = "markdown",
+		keys = {
+			{
+				"<leader>op",
+				ft = "markdown",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				desc = "Markdown Preview",
+			},
+		},
+		init = function()
+			vim.g.mkdp_page_title = "${name}"
+		end,
+	},
+}
