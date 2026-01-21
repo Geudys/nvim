c2b82d1821c7d4d088a572a7bea333b1e2dec79a
Geudys <geudysperezstgo@gmail.com>
2026-01-21 19:12:10 -0400

    update-cmp-config
    remove: kind icons for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -26,34 +26,6 @@ return {
 					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
-			local kind_icons = {
-				Text = "",
-				Method = "󰆧",
-				Function = "󰊕",
-				Constructor = "",
-				Field = "󰇽",
-				Variable = "󰂡",
-				Class = "󰠱",
-				Interface = "",
-				Module = "",
-				Property = "󰜢",
-				Unit = "",
-				Value = "󰎠",
-				Enum = "",
-				Keyword = "󰌋",
-				Snippet = "",
-				Color = "󰏘",
-				File = "󰈙",
-				Reference = "󰈇",
-				Folder = "󰉋",
-				EnumMember = "",
-				Constant = "󰏿",
-				Struct = "󰙅",
-				Event = "",
-				Operator = "󰆕",
-				TypeParameter = "",
-			}
-
 			cmp.setup({
 				window = {
 					completion = cmp.config.window.bordered({
@@ -119,14 +91,6 @@ return {
 						end
 					end, { "i", "s" }),
 				}),
-
-				formatting = {
-					fields = { "abbr", "kind" },
-					format = function(_, vim_item)
-						vim_item.kind = kind_icons[vim_item.kind] or ""
-						return vim_item
-					end,
-				},
 
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
