070943dcc41f41be449361b1660907138097934d
Geudys <geudysperezstgo@gmail.com>
2026-01-23 22:09:36 -0400

    update-tab-size
    update: tab size 4 -> 8

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -13,9 +13,9 @@ vim.opt.cursorline = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -34,19 +34,6 @@ return {
 					yml = { "prettier" },
 					python = { "black", "isort" },
 				},
-				formatters = {
-					prettier = {
-						prepend_args = {
-							"--tab-width=4",
-							"--use-tabs=false",
-							"--single-quote=true",
-							"--trailing-comma=es5",
-							"--semi=true",
-							"--bracket-spacing=true",
-							"--arrow-parens=avoid",
-						},
-					},
-				},
 				default_format_opts = { lsp_fallback = true },
 				format_on_save = { timeout_ms = 3000, lsp_fallback = true },
 			})
