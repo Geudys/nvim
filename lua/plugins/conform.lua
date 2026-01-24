00986b3d300939495e961989809e5c26db5a0531
Geudys <geudysperezstgo@gmail.com>
2026-01-24 12:19:03 -0400

    update-conform-plugins
    update: tabs size for pretierd 'default'-> 8

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -34,6 +34,19 @@ return {
 					yml = { "prettier" },
 					python = { "black", "isort" },
 				},
+				formatters = {
+					prettier = {
+						prepend_args = {
+							"--tab-width=8",
+							"--use-tabs=true",
+							"--single-quote=true",
+							"--trailing-comma=es5",
+							"--semi=true",
+							"--bracket-spacing=true",
+							"--arrow-parens=avoid",
+						},
+					},
+				},
 				default_format_opts = { lsp_fallback = true },
 				format_on_save = { timeout_ms = 3000, lsp_fallback = true },
 			})
