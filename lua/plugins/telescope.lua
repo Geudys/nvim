e0bd6b73b8e8db469d15787295549ee574ac5c54
Geudys <geudysperezstgo@gmail.com>
2026-01-28 13:43:59 -0400

    update-telescope-config
    add: fzf native in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,10 @@ return {
 		"nvim-telescope/telescope.nvim",
 		lazy = false,
 		cmd = "Telescope",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
 
 		config = function()
 			local telescope = require("telescope")
