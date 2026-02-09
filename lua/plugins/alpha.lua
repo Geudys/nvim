6d9a726eb5b02edfd7ebe39c3c17ab52f510a4e1
Geudys <geudysperezstgo@gmail.com>
2026-02-08 20:24:05 -0400

    disable-alpha-plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,34 +1,34 @@
 return {
-    {
-        "goolord/alpha-nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            local alpha = require("alpha")
-            local dashboard = require("alpha.themes.dashboard")
-
-            dashboard.section.header.val = {
-                [[                                                                      ]],
-                [[       ████ ██████           █████      ██                     ]],
-                [[      ███████████             █████                             ]],
-                [[      █████████ ███████████████████ ███   ███████████   ]],
-                [[     █████████  ███    █████████████ █████ ██████████████   ]],
-                [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-                [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-                [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-            }
-
-            dashboard.section.buttons.val = {
-                dashboard.button("e", "  Yazi", ":Yazi<CR>"),
-                dashboard.button("f", "  Search files", ":Telescope find_files<CR>"),
-                dashboard.button("r", "  Recents", ":Telescope oldfiles<CR>"),
-                dashboard.button("h", "  Check health", ":checkhealth<CR>"),
-                dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
-                dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
-                dashboard.button("q", "  Exit", ":qa<CR>"),
-            }
-
-            dashboard.opts.opts.noautocmd = true
-            alpha.setup(dashboard.config)
-        end,
-    },
+	-- {
+	--     "goolord/alpha-nvim",
+	--     dependencies = { "nvim-tree/nvim-web-devicons" },
+	--     config = function()
+	--         local alpha = require("alpha")
+	--         local dashboard = require("alpha.themes.dashboard")
+	--
+	--         dashboard.section.header.val = {
+	--             [[                                                                      ]],
+	--             [[       ████ ██████           █████      ██                     ]],
+	--             [[      ███████████             █████                             ]],
+	--             [[      █████████ ███████████████████ ███   ███████████   ]],
+	--             [[     █████████  ███    █████████████ █████ ██████████████   ]],
+	--             [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+	--             [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+	--             [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+	--         }
+	--
+	--         dashboard.section.buttons.val = {
+	--             dashboard.button("e", "  Yazi", ":Yazi<CR>"),
+	--             dashboard.button("f", "  Search files", ":Telescope find_files<CR>"),
+	--             dashboard.button("r", "  Recents", ":Telescope oldfiles<CR>"),
+	--             dashboard.button("h", "  Check health", ":checkhealth<CR>"),
+	--             dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+	--             dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
+	--             dashboard.button("q", "  Exit", ":qa<CR>"),
+	--         }
+	--
+	--         dashboard.opts.opts.noautocmd = true
+	--         alpha.setup(dashboard.config)
+	--     end,
+	-- },
 }
