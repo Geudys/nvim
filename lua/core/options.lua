204758702e20e51cf6f8cd1662ea38a750318752
Geudys <geudysperezstgo@gmail.com>
2026-02-10 20:20:26 -0400

    fix options config
    fix: star treesiter for txt files

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -38,7 +38,7 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "markdown", "text" },
+	pattern = { "markdown" },
 	callback = function()
 		vim.opt_local.wrap = true
 		vim.opt_local.linebreak = true
