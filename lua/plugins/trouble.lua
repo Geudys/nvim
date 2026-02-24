b70511d51f29494b1bffe29ae237c998500d9aac
Geudys <geudysperezstgo@gmail.com>
2026-02-24 09:57:05 -0400

    update-config-trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,6 +4,7 @@ return {
 	cmd = "Trouble",
 	opts = {
 		win = {
+			position = "left",
 			size = 40,
 		},
 		use_diagnostic_signs = true,
@@ -15,7 +16,7 @@ return {
 		map(
 			"n",
 			"<leader>xx",
-			"<cmd>Trouble toggle win.position=left<cr>",
+			"<cmd>Trouble toggle<cr>",
 			vim.tbl_extend("force", opts, {
 				desc = "Trouble toggle",
 			})
@@ -24,25 +25,25 @@ return {
 		map(
 			"n",
 			"<leader>xd",
-			"<cmd>Trouble diagnostics toggle win.position=left<cr>",
+			"<cmd>Trouble diagnostics toggle<cr>",
 			vim.tbl_extend("force", opts, {
-				desc = "Diagnostics",
+				desc = "Diagnostics (workspace)",
 			})
 		)
 
 		map(
 			"n",
 			"<leader>xb",
-			"<cmd>Trouble diagnostics toggle filter.buf=0 win.position=left<cr>",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
 			vim.tbl_extend("force", opts, {
-				desc = "Buffer diagnostics",
+				desc = "Diagnostics (buffer)",
 			})
 		)
 
 		map(
 			"n",
 			"<leader>xs",
-			"<cmd>Trouble symbols toggle win.position=left<cr>",
+			"<cmd>Trouble symbols win.position=left<cr>",
 			vim.tbl_extend("force", opts, {
 				desc = "Symbols / Outline",
 			})
@@ -51,9 +52,36 @@ return {
 		map(
 			"n",
 			"<leader>xr",
-			"<cmd>Trouble lsp togglewin.position=left<cr>",
+			"<cmd>Trouble lsp_references toggle<cr>",
 			vim.tbl_extend("force", opts, {
 				desc = "LSP references",
+			})
+		)
+
+		map(
+			"n",
+			"<leader>xd",
+			"<cmd>Trouble lsp_definitions toggle<cr>",
+			vim.tbl_extend("force", opts, {
+				desc = "LSP definitions",
+			})
+		)
+
+		map(
+			"n",
+			"<leader>xi",
+			"<cmd>Trouble lsp_implementations toggle<cr>",
+			vim.tbl_extend("force", opts, {
+				desc = "LSP implementations",
+			})
+		)
+
+		map(
+			"n",
+			"<leader>xt",
+			"<cmd>Trouble lsp_type_definitions toggle<cr>",
+			vim.tbl_extend("force", opts, {
+				desc = "LSP type definitions",
 			})
 		)
 	end,
