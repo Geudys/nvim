b6175dd8ed71f1a9c945473a89f6404eee402a26
Geudys <geudysperezstgo@gmail.com>
2026-02-27 14:25:21 -0400

    add-lint-plugins

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+
+			lint.linters_by_ft = {
+				python = { "flake8" },
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				lua = { "luacheck" },
+			}
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+}
