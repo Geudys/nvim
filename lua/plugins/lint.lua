74c76c83078ca5a72d45fd8067410285392d2c78
Geudys <geudysperezstgo@gmail.com>
2026-03-03 22:37:36 -0400

    fix-lint-config
    add: arguments for luacheck

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -12,6 +12,23 @@ return {
 				lua = { "luacheck" },
 			}
 
+			lint.linters.luacheck = {
+				cmd = "luacheck",
+				stdin = true,
+				args = {
+					"--globals",
+					"vim",
+					"lvim",
+					"reload",
+					"--",
+				},
+				stream = "stdout",
+				ignore_exitcode = true,
+				parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
+					source = "luacheck",
+				}),
+			}
+
 			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 				callback = function()
 					lint.try_lint()
