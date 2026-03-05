a2f1f70d4573c5f06ec478adb5af7ae7a0daf6cf
Geudys <geudysperezstgo@gmail.com>
2026-03-04 21:43:56 -0400

    update-lsp-config
    add: config for eslint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -123,6 +123,16 @@ return {
 			},
 		})
 
+		vim.lsp.config("eslint", {
+			capabilities = capabilities,
+			on_attach = function(_, bufnr)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					command = "EslintFixAll",
+				})
+			end,
+		})
+
 		vim.lsp.config("rust_analyzer", {
 			capabilities = capabilities,
 			settings = {
