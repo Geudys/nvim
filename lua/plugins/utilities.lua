09d36fb7f5e92446d2c6e7886cbac4312a87263d
Geudys <geudysperezstgo@gmail.com>
2026-03-04 22:01:15 -0400

    migrate-utilites-plugins
    migrate: several plugins migrated to 'coding' files (flash, undotree,
    nvim-ufo)

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -1,29 +1,17 @@
 return {
-	{
-		"folke/flash.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		config = function()
-			require("flash").setup({
-				search = { case_sensitive = false, smart_case = false, incremental = false },
-			})
-			vim.keymap.set({ "n", "x", "o" }, "s", function()
-				require("flash").jump()
-			end, { desc = "Flash search" })
-		end,
-	},
+
 	{
 		"folke/which-key.nvim",
 		event = false,
 		opts = {
 			preset = "helix",
-			delay = 250,
 			win = {
 				border = "rounded",
 				padding = { 0.5, 1 },
 			},
 		},
 	},
+
 	{
 		"xiyaowong/nvim-transparent",
 		lazy = false,
@@ -60,6 +48,7 @@ return {
 			vim.cmd("TransparentEnable")
 		end,
 	},
+
 	{
 		"rcarriga/nvim-notify",
 		lazy = true,
@@ -68,60 +57,6 @@ return {
 			local notify = require("notify")
 			notify.setup({ background_colour = "#000001", timeout = 200 })
 			vim.notify = notify
-		end,
-	},
-	{
-		"mbbill/undotree",
-		lazy = true,
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			vim.g.undotree_WindowLayout = 3
-
-			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
-		end,
-	},
-	{
-		"kaarmu/typst.vim",
-		event = "VeryLazy",
-		ft = "typst",
-	},
-
-	{
-		"chomosuke/typst-preview.nvim",
-		lazy = false,
-		version = "1.*",
-		opts = {},
-	},
-
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		event = "VeryLazy",
-		opts = {
-			signs = true,
-			sign_priority = 8,
-
-			keywords = {
-				FIXME = { icon = "", color = "error", alt = { "BUG", "FIX", "BROKEN" } },
-				SECURITY = { icon = "󰒃", color = "error" },
-				TODO = { icon = "", color = "warning" },
-				HACK = { icon = "", color = "warning" },
-				OPTIMIZE = { icon = "󰓅", color = "warning", alt = { "PERF", "PERFORMANCE" } },
-				NOTE = { icon = "󰍨", color = "info", alt = { "INFO" } },
-				REVIEW = { icon = "󰆈", color = "hint" },
-				DEPRECATED = { icon = "󰂭", color = "warning" },
-				REFACTOR = { icon = "󰑓", color = "hint" },
-			},
-			vim.keymap.set("n", "<leader>tt", "<cmd>TodoTelescope<CR>", { desc = "TODO comments" }),
-			vim.keymap.set("n", "<leader>xt", "<cmd>TodoTrouble win.position=left<CR>", { desc = "TODO comments" }),
-		},
-	},
-	{
-		"MagicDuck/grug-far.nvim",
-		config = function()
-			require("grug-far").setup({
-				vim.keymap.set("n", "<leader>R", "<cmd>GrugFar<CR>", { desc = "Grug Far" }),
-			})
 		end,
 	},
 	{
@@ -141,24 +76,6 @@ return {
 				css_fn = true,
 				mode = "background",
 			})
-		end,
-	},
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = {
-			"kevinhwang91/promise-async",
-		},
-		config = function()
-			require("ufo").setup({
-				provider_selector = function()
-					return { "treesitter", "indent" }
-				end,
-			})
-
-			vim.o.foldcolumn = "0"
-			vim.o.foldlevel = 99
-			vim.o.foldlevelstart = 99
-			vim.o.foldenable = true
 		end,
 	},
 }
