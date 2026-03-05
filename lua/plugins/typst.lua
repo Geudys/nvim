3a92e83f8784b4b443d8dacf8e705b4b312d58a2
Geudys <geudysperezstgo@gmail.com>
2026-03-04 21:56:27 -0400

    add-typst-plugins-file
    info: migrate typst plugins (utilites -> typst)

---
+++ lua/plugins/typst.lua
@@ -0,0 +1,15 @@
+return {
+
+	{
+		"kaarmu/typst.vim",
+		event = "VeryLazy",
+		ft = "typst",
+	},
+
+	{
+		"chomosuke/typst-preview.nvim",
+		lazy = false,
+		version = "1.*",
+		opts = {},
+	},
+}
