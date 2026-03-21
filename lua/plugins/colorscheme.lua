3cb11e550ee836bdbf375608ea21e69ff615c482
Geudys <geudysperezstgo@gmail.com>
2026-03-20 21:17:18 -0400

    change-colorcheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,7 +8,7 @@ return {
 
 	{
 		"rose-pine/neovim",
-		lazy = false,
+		lazy = true,
 		name = "rose-pine",
 		config = function()
 			local rose = require("rose-pine")
@@ -23,20 +23,20 @@ return {
 		end,
 	},
 
-	-- {
-	-- 	"oxfist/night-owl.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		local night_owl = require("night-owl")
-	-- 		night_owl.setup({
-	-- 			bold = true,
-	-- 			italics = true,
-	-- 			underline = true,
-	-- 			undercurl = true,
-	-- 			transparent_background = true,
-	-- 		})
-	-- 		vim.cmd("colorscheme night-owl")
-	-- 	end,
-	-- },
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local night_owl = require("night-owl")
+			night_owl.setup({
+				bold = true,
+				italics = true,
+				underline = true,
+				undercurl = true,
+				transparent_background = true,
+			})
+			vim.cmd("colorscheme night-owl")
+		end,
+	},
 }
