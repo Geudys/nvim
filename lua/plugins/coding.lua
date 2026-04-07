7dd22648071daee25a93daf583742fa37e62c9c6
Geudys <geudysperezstgo@gmail.com>
2026-04-06 22:14:54 -0400

    remove(plugins): undotree discard for new version

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -14,17 +14,6 @@ return {
 	},
 
 	{
-		"mbbill/undotree",
-		lazy = true,
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			vim.g.undotree_WindowLayout = 3
-
-			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
-		end,
-	},
-
-	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		event = "VeryLazy",
